c96f067b762527b7152d6049c16ed40b7cbe12e4
leosantosw <leonardosantosx0077@gmail.com>
2023-07-14 22:06:44 -0300

    feat: create notification use case

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,1 +1,28 @@
-export class SendNotification {}
+import { Content } from '../entities/content'
+import { Notification } from '../entities/notification'
+
+interface SendNotificationRequest {
+  recipientId: string
+  content: string
+  category: string
+}
+
+interface SendNotificationResponse {
+  notification: Notification
+}
+
+export class SendNotification {
+  async execute(
+    request: SendNotificationRequest
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request
+
+    const notification = new Notification({
+      recipientId,
+      category,
+      content: new Content(content),
+    })
+
+    return { notification }
+  }
+}
