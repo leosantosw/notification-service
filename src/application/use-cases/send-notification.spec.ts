be625fd46e0cc74795dbdd4ec7b317f5eeba688c
leosantosw <leonardosantosx0077@gmail.com>
2023-07-14 22:29:03 -0300

    feat: create notification repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,14 +1,23 @@
 import { SendNotification } from './send-notification'
+import { Notification } from '../entities/notification'
+
+const notifications: Notification[] = []
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification)
+  },
+}
 
 describe('SendNotification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification()
-    const { notification } = await sendNotification.execute({
+    const sendNotification = new SendNotification(notificationsRepository)
+    await sendNotification.execute({
       recipientId: 'example',
       content: 'content',
       category: 'category',
     })
 
-    expect(notification).toBeTruthy()
+    expect(notifications).toHaveLength(1)
   })
 })
