1a1172247818c43dff10063722b3b4c4be21ce12
leosantosw <leonardosantosx0077@gmail.com>
2023-07-16 13:53:47 -0300

    chore: add path mapper

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
-import { Notification } from '../../../../application/entities/notification'
-import { NotificationsRepository } from '../../../../application/repositories/notifications-repository'
 import { PrismaService } from '../prisma.service'
+import { Notification } from '@application/entities/notification'
+import { NotificationsRepository } from '@application/repositories/notifications-repository'
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
